dd96f63df3262d1e8337ba27d88bfd65f5f9e633
Genzaiwuxian <31791192+Genzaiwuxian@users.noreply.github.com>
2018-05-03 23:02:24 +0800

    1

--- src/main.cpp
+++ src/main.cpp
@@ -64,9 +64,9 @@ int main()
 		  
 		  pid.UpdateError(cte);
 		  steer_value = pid.TotalError();
-		  if (steer_value > 0.3)
+		  if (steer_value > 0.35)
 			  steer_value = 1;
-		  if (steer_value < -0.3)
+		  if (steer_value < -0.35)
 			  steer_value = -1;
 
 		  pid.InitThrottle(0.1, 20);
