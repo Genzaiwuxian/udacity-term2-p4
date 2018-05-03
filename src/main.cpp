c4805edc0fa97fbee4ea7845b607d9a776a9f099
Genzaiwuxian <31791192+Genzaiwuxian@users.noreply.github.com>
2018-05-03 21:41:49 +0800

    1

--- src/main.cpp
+++ src/main.cpp
@@ -34,9 +34,9 @@ int main()
 
   PID pid;
   // TODO: Initialize the pid variable.
-  const double init_Kp = 0.15;
+  const double init_Kp = 0.09;
   const double init_Ki = 0.0001;
-  const double init_Kd = 0.6;
+  const double init_Kd = 0.1;
   pid.Init(init_Kp, init_Ki, init_Kd);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
